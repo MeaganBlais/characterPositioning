8011ecff97e64ffb23471c0a93eb64dff62573bf
Meagan Blais <meaganblais@gmail.com>
2017-05-31 15:16:03 -0400

    submitting a cleaner output version

--- charPositioning.js
+++ charPositioning.js
@@ -1,15 +1,20 @@
 function countLetters(str) {
-    var output = { };
-    splitStr = str.split(' ').join('').toLowerCase().split('')
+    let output = { };
+    splitStr = str.split('').join('').toLowerCase();
 
     for (var i = 0; i < splitStr.length; i++) {
         var currChar = splitStr[i];
         if (output[currChar] === undefined) {
-            output[currChar] = [i] // when using this in string form multiples of i are returned
+            var num = [i]; // try and clean this up more
+            var convertedNum = num.toString();
+            output[currChar] = convertedNum;
+            // output[currChar] = [i]; // when using this in string form multiples of i are returned
         } else {
-            output[currChar] = output[currChar] + ', ' + [i]
+        //    output[currChar] = output[currChar] + ', ' + [i]
+           output[currChar] += ', ' + [i];
         }
     }
+        delete output[' '];
         return output;
 }
 
